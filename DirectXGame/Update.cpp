a07ff0d7a41e95c9ed77bdf4c0759cf8a0fa09d3
sakurabakirato <k024g0054@g.neec.ac.jp>
2026-08-03 12:29:16 +0900

    コミット

---
+++ DirectXGame/Update.cpp
@@ -0,0 +1,11 @@
+#include "UpDate.h"
+
+using namespace KamataEngine;
+
+void UpDate::WorldTransformUpData(WorldTransform& worldTransform) 
+{
+	// スケール、回転、平行移動を合成して行列を計算する
+	worldTransform.matWorld_ = MakeAffineMatrix(worldTransform.scale_, worldTransform.rotation_, worldTransform.translation_);
+
+	worldTransform.TransferMatrix();
+}
